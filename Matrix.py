d7c21eb0cefbf086c53eac06f82a2d08ab6b576a
mickle0629 <80008714+mickle0629@users.noreply.github.com>
2022-03-22 22:46:01 -0700

    added __str__

--- Matrix.py
+++ Matrix.py
@@ -1,15 +1,22 @@
 class Matrix:
     elements = []
-
+    '''
+        Constructor.
+    '''
     def __init__(self, num_row, num_col):
         for i in range(num_row):
             columns = []
             for j in range(num_col):
                 columns.append(float(input(f"Enter the value for element ({i}, {j}):")))
             self.elements.append(columns)
-    #TODO: print() override
-    #def __str__(self):
-    #    for i in range(len(self.elements)):
+
+
+    def __str__(self):
+        element_str_rep = ""
+        for row in self.elements:
+            element_str_rep += str(row) + "\n"
+        return element_str_rep
+
 
 
 
